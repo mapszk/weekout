594ae9c49e5a6c9bdcc569979418567c3420abd2
Martin Pilaszek <martinpilaszek@gmail.com>
2021-08-06 12:43:38 -0300

    correccion en firebaseServer para que tome la privateKey

--- util/firebaseServer.ts
+++ util/firebaseServer.ts
@@ -1,13 +1,18 @@
 import * as admin from "firebase-admin"
 
 if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
-    }),
-  })
+  if (process.env.FIREBASE_ADMIN_PRIVATE_KEY) {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(
+          /\\n/g,
+          "\n"
+        ),
+      }),
+    })
+  }
 } else admin.app()
 
 export const adminDb = admin.firestore()
